183ad767c22d37a29319f108997a3f19235babae
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-05-03 15:20:19 +0300

    Create config.py
    Создан конфиг приложения

---
+++ app/config.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+
+import dotenv
+
+dotenv.load_dotenv(Path('.env'))
+
+
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASS = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+
+
+DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+DATE_FORMAT = "%d.%m.%Y"
