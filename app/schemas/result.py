527f7491ec7e5bf159e50e89f04a578ca034bc1d
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-05-03 15:25:37 +0300

    Create result.py
    Добавлена схема для работы с данными из модели Result

---
+++ app/schemas/result.py
@@ -0,0 +1,32 @@
+from datetime import date, datetime
+from typing import Annotated
+
+from pydantic import BaseModel, Field
+
+
+class ResultSchema(BaseModel):
+    id: int
+
+    exchange_product_id: Annotated[str, Field(max_length=50)]
+
+    exchange_product_name: Annotated[str, Field(max_length=500)]
+
+    oil_id: Annotated[str, Field(max_length=4)]
+
+    delivery_basis_id: Annotated[str, Field(max_length=3)]
+
+    delivery_type_id: Annotated[str, Field(max_length=1)]
+
+    delivery_basis_name: Annotated[str, Field(max_length=255)]
+
+    volume: int
+
+    total: int
+
+    count: int
+
+    date: date
+
+    created_on: datetime
+
+    updated_on: datetime
