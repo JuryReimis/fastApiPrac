762b06a7c894afe75f78f5954e478bb08470dfff
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-05-04 19:17:13 +0300

    Добавлен lifespan для инициализации задач

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
+from app.cache.sheduler_tasks import SchedulerTasks
 from app.views import trading_views
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Запускаем планировщик при старте
+    scheduler = SchedulerTasks()
+    scheduler.make_clear_cache_task()
+
+    yield
+
+    # Остановка планировщика при завершении
+    scheduler.shutdown(wait=False)
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(trading_views.router)
 
