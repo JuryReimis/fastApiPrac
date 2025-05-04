2f05b86f817906a5c234f4efa4b1bcd07770133f
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-05-04 19:10:09 +0300

    Схема для передачи конфига redis

---
+++ app/schemas/redis.py
@@ -0,0 +1,10 @@
+from typing import Optional, Annotated
+
+from pydantic import BaseModel, Field, RedisDsn
+
+
+class RedisCacheConfigSchema(BaseModel):
+    host: Annotated[str, Field(description="Хост Redis")] = 'localhost'
+    port: Annotated[int, Field(description="Порт Redis")] = 6379
+    dsn: Annotated[RedisDsn | None, Field(description="URL подключения (redis://...)")] = None
+    default_ttl: Annotated[int | None, Field(description="TTL по умолчанию (секунды)")] = None
